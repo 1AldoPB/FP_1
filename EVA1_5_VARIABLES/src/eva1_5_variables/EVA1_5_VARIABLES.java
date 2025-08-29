5430c36e972aa79f9d105248b1398cd1068e5231
1AldoPB <L25550357@chihuahua2.tecnm.mx>
2025-08-28 22:20:09 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,26 @@
+
+package eva1_5_variables;
+
+public class EVA1_5_VARIABLES {
+
+   
+    public static void main(String[] args) {
+        // SIEMPRE DECLARAR LAS VARIABLES PRIMERO
+        //Tipo de dato Luego el nombre del identificador    
+        String nombre;
+        int edad;
+        double salario;
+        //INICIALIZACION
+        //IDENTIFICADOR=VALOR;
+        //=OPERADOR DE ASIGNACION
+        nombre= "RUBEN ALONSO HERNANDEZ";
+        edad=43;
+        salario=1.1;
+        //edad= "ruben"; ESTA MAL 
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+        
+    }
+    
+}
