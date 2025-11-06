5bad832f1cd6bdbe175844fd42064b2edffd999d
1AldoPB <L25550357@chihuahua2.tecnm.mx>
2025-11-06 16:51:31 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_metodos;
+
+/**
+ *
+ * @author aldop
+ */
+public class EVA3_1_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int val1=5,val2=10;
+      int resu=sumaEnteros(val1,val2);
+    }
+    //METODO PARA SUMAR 2 ENTEROS
+    //LLAMAR A SUS METODOS USANDO VERBOS Y ESCRITURA camelCase
+    //1ERO MODIFICADORES DE ACCESO: public,protected,private,default
+    //2.static --> PARA USAR LOS METODOS DENTRO DEL MAIN
+    //3.VALOR DE RETORNO:CUALQUIER TIPO DE DATO
+    //VOID -->si no quieren regresar un valor
+    //4.NOMBRE DEL METODO:MIM¿SMAS REGLAS QUE IDENTIFICADORES
+    //5.LISTA DE PARAMETROS:PUEDEDN SER 0 O N CANTIDAD
+    //1      2     3      4                 5
+    public static int sumaEnteros(int num1,int num2){
+int suma = num1 + num 2;   
+int r
+  return suma;
+          }
+}
